a68e654db7c9a1ef78815fab63b0fae10a2a3de2
kdorr <kimberly.orr97@gmail.com>
2018-08-13 16:17:38 -0500

    Convert _set_limits

--- mplaltair/tests/test_axis.py
+++ mplaltair/tests/test_axis.py
@@ -64,6 +64,7 @@ def test_axis_zero_quantitative(x, y, zero):
     )
     fig, ax = convert(chart)
     return fig
+    # plt.show()
 
 
 @pytest.mark.mpl_image_compare(baseline_dir='baseline_images/test_axis')
